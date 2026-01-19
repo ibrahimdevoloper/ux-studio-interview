d4f08d31d7de6050e889ce1d318954759d759c83
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2026-01-19 23:10:53 +0300

    Added api Exception Handler.

--- backend/src/main/kotlin/com/ibrahim/interview/Exception/ApiExceptionHandler.kt
+++ backend/src/main/kotlin/com/ibrahim/interview/Exception/ApiExceptionHandler.kt
@@ -2,20 +2,21 @@ package com.ibrahim.interview.Exception
 
 import com.google.cloud.storage.StorageException
 import jakarta.persistence.EntityNotFoundException
-import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
-@ControllerAdvice
-class ExceptionHandler {
+@RestControllerAdvice
+class ApiExceptionHandler{
+
 
     @ExceptionHandler(CustomException::class)
-    fun handleCustomException(ex: CustomException, request: WebRequest): ResponseEntity<ApiError> {
+    fun handleCustomException(ex: CustomException, request: WebRequest):
+            ResponseEntity<ApiError> {
         val response: ApiError = ApiError(
             "CustomException",
             "Bad Request",
@@ -23,15 +24,20 @@ class ExceptionHandler {
             ex.message,
             request.getContextPath()
         )
-        ex.printStackTrace()
-        if (ex.status == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body<ApiError>(response)
-        }
-        return ResponseEntity.status(ex.status?:HttpStatus.BAD_REQUEST).body<ApiError>(response)
+//        ex.printStackTrace()
+//        if (ex.status == null) {
+//            return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body<ApiError>(response)
+//        }
+        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body<ApiError>(response)
+
+//        return ResponseEntity.status(ex.status?:HttpStatus.BAD_REQUEST).body<ApiError>(response)
     }
 
     @ExceptionHandler(EntityNotFoundException::class)
-    fun handleRecordNotFound(ex: EntityNotFoundException, request: WebRequest): ResponseEntity<ApiError> {
+    fun handleRecordNotFound(ex: EntityNotFoundException, request: WebRequest):
+            ResponseEntity<ApiError> {
         val response: ApiError = ApiError(
             "EntityNotFoundException",
             "Not Found",
@@ -39,7 +45,8 @@ class ExceptionHandler {
             ex.message,
             request.getContextPath()
         )
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body<ApiError>(response)
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body<ApiError>(response)
+        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(StorageException::class)
@@ -49,11 +56,12 @@ class ExceptionHandler {
     ): ResponseEntity<ApiError> {
         val response: ApiError = ApiError(
             "FirebaseStorageException",
-            ex?.message?:"Firebase Storage Error Acurred",
+            ex?.message ?: "Firebase Storage Error Acurred",
             HttpStatus.BAD_REQUEST.value(),
             "Firebase Storage Error Acurred",
             request.getContextPath()
         )
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body<ApiError>(response)
+        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body<ApiError>(response)
     }
 }
