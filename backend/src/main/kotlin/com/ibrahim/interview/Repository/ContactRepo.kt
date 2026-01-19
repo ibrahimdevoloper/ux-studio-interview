6a57f4e13c2fb1701a9deb5b88d62803e86d8b72
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2026-01-19 22:54:07 +0300

    Added Contact get all query.

--- backend/src/main/kotlin/com/ibrahim/interview/Repository/ContactRepo.kt
+++ backend/src/main/kotlin/com/ibrahim/interview/Repository/ContactRepo.kt
@@ -1,8 +1,12 @@
 package com.ibrahim.interview.Repository
 
 import com.ibrahim.interview.Entity.Contact
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ContactRepo: CrudRepository<Contact, Int>
+interface ContactRepo: CrudRepository<Contact, Int>{
+    @Query("SELECT c FROM Contact as c")
+    fun getAllContact(): List<Contact>
+}
