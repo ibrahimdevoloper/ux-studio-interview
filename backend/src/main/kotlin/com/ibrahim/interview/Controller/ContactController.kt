05a8fe43c27728e072f7a6768840191cebb26e8f
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2026-01-21 15:39:46 +0300

    added CrosOrigin anotiation.

--- backend/src/main/kotlin/com/ibrahim/interview/Controller/ContactController.kt
+++ backend/src/main/kotlin/com/ibrahim/interview/Controller/ContactController.kt
@@ -6,6 +6,7 @@ import com.ibrahim.interview.Service.ContactService
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController()
 @RequestMapping("api/v1/contacts")
+@CrossOrigin
 class ContactController(val contactService: ContactService) {
 
     @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
--- backend/src/main/kotlin/com/ibrahim/interview/Exception/ApiExceptionHandler.kt
+++ backend/src/main/kotlin/com/ibrahim/interview/Exception/ApiExceptionHandler.kt
@@ -5,12 +5,14 @@ import jakarta.persistence.EntityNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
+@CrossOrigin
 class ApiExceptionHandler{
 
 
